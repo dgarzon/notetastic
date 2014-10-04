96b53a91d35c43bd6e22b936b3ec30c4de852119
Daniel Garzon <dg2796@columbia.edu>
2014-10-04 02:47:27 -0400

    Initial Commit

---
+++ Notetastic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notetastic
+//
+//  Created by Daniel Garzon on 10/4/14.
+//  Copyright (c) 2014 Daniel Garzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Notetastic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notetastic
+//
+//  Created by Daniel Garzon on 10/4/14.
+//  Copyright (c) 2014 Daniel Garzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
